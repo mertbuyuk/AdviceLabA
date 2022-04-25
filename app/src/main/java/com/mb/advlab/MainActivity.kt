dfddccf8c924a5e1858be0a221fd7e888e5c44af
KENTKART\mert.buyuk <mertbuyuk.e@gmail.com>
2022-04-25 11:41:02 +0300

    navigations with nav comp

--- app/src/main/java/com/mb/advlab/MainActivity.kt
+++ app/src/main/java/com/mb/advlab/MainActivity.kt
@@ -2,14 +2,59 @@ package com.mb.advlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mb.advlab.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var navView : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        supportActionBar?.hide()
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+
+        navView = binding.navView
+        navView.setupWithNavController(navController)
+
+        var appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.profileFragment,
+                R.id.homeFragment
+            )
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.signupFragment -> hideBottomNav()
+                R.id.loginFragment -> hideBottomNav()
+                else -> showBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.navView.visibility = View.VISIBLE
+
+    }
+
+    private fun hideBottomNav() {
+        binding.navView.visibility  = View.GONE
     }
 }
